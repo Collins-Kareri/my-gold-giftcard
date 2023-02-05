ed65ee0958c8e480478e33462123aa7bce6fb4ab
Collins Kareri <locoreri@gmail.com>
2023-02-05 07:09:09 +0300

    functionality determine what checkout ui will be.

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,12 +1,15 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, NavigateFunction } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Login from "~/pages/login";
 
-function CheckOut() {
-    const navigate = useNavigate();
-
+function LoggedIn({ navigate }: { navigate: NavigateFunction }) {
 	return (
-        <div className="tw-flex tw-justify-center tw-items-center tw-flex-col tw-w-full tw-h-screen">
-            <button className="tw-capitalize tw-fixed tw-top-8 tw-left-12 tw-font-bold" onClick={()=>navigate("/")}><FontAwesomeIcon icon={"caret-left"}/> back</button>
+		<div className="tw-flex tw-justify-center tw-items-center tw-flex-col tw-w-full tw-h-screen">
+			<button
+				className="tw-capitalize tw-fixed tw-top-8 tw-left-12 tw-font-bold"
+				onClick={() => navigate("/")}>
+				<FontAwesomeIcon icon={"caret-left"} /> back
+			</button>
 			<p className="tw-capitalize tw-text-xl tw-mb-4">pay with</p>
 			<button className="tw-capitalize tw-w-fit tw-border tw-border-slate-800 tw-py-2 tw-px-4 tw-text-slate-50 tw-bg-slate-900 tw-mb-4">
 				paypal
@@ -14,10 +17,18 @@ function CheckOut() {
 			<p className="tw-mb-4">or</p>
 			<button className="tw-capitalize tw-w-fit tw-border tw-border-slate-800 tw-py-2 tw-px-4 tw-text-slate-50 tw-bg-slate-900">
 				bitpay
-            </button>
-            <p className="tw-font-bold tw-uppercase tw-mt-2">we support bitcoin, ethereum and usdt</p>
+			</button>
+			<p className="tw-font-bold tw-uppercase tw-mt-2">
+				we support bitcoin, ethereum and usdt
+			</p>
 		</div>
 	);
+}
+
+function CheckOut() {
+	const navigate = useNavigate();
+
+	return <Login />;
 }
 
 export default CheckOut;
