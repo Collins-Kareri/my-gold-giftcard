454304001318192a4f8c0a31bc527bcda7c8ad41
Collins Kareri <locoreri@gmail.com>
2023-02-05 11:07:26 +0300

    Display notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab,faGoogle } from "@fortawesome/free-brands-svg-icons";
-import { fas, faMagnifyingGlass,faShoppingBasket,faUserLarge,faCaretDown,faXmark,faCaretLeft } from "@fortawesome/free-solid-svg-icons";
-import NavBar from '~/components/navbar';
+import { fab, faGoogle } from "@fortawesome/free-brands-svg-icons";
+import {
+	fas,
+	faMagnifyingGlass,
+	faShoppingBasket,
+	faUserLarge,
+	faCaretDown,
+	faXmark,
+	faCaretLeft,
+	faInfo,
+	faExclamation,
+	faExplosion,
+	faCheckDouble,
+} from "@fortawesome/free-solid-svg-icons";
+import NavBar from "~/components/navbar";
 import ErrorPage from "~/pages/errorPage";
-import Home from '~/pages/home';
+import Home from "~/pages/home";
 import Login from "~/pages/login";
 import SignUp from "~/pages/signup";
 import SellCards from "~/pages/sellCards";
 import Results from "~/pages/results";
 import CheckOut from "./pages/checkout";
+import NotificationList from "~/components/notifications/notificationsList";
 import store from "~/redux/store";
 import { Provider as StoreProvider } from "react-redux";
 
 function RootOfProject() {
 	return (
 		<>
+			<NotificationList />
 			<NavBar>
 				<Outlet />
 			</NavBar>
@@ -66,7 +80,11 @@ library.add(
 	faCaretDown,
 	faXmark,
 	faGoogle,
-	faCaretLeft
+	faCaretLeft,
+	faInfo,
+	faExclamation,
+	faExplosion,
+	faCheckDouble
 );
 
 function App() {
