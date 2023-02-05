4116fba9cc9095d9a5efe1a59982856f6b65a3f4
Collins Kareri <locoreri@gmail.com>
2023-02-05 11:09:11 +0300

    add notification reducers to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "~/redux/cartSlice";
+import cartReducer from "~/redux/slice/cartSlice";
+import noticationReducer from "~/redux/slice/notificationsSlice";
 
-export default configureStore({
+const store = configureStore({
 	reducer: {
 		cart: cartReducer,
+		notification: noticationReducer,
 	},
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
