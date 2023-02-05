1f93c660861d5b7f9df8ec85157d82207173196d
Collins Kareri <locoreri@gmail.com>
2023-02-05 11:08:52 +0300

    Determine what to render on status message absent.

--- src/pages/errorPage.tsx
+++ src/pages/errorPage.tsx
@@ -1,15 +1,28 @@
 import { useRouteError } from "react-router-dom";
 
-export default function ErrorPage() {
-    const error = useRouteError() as { status: number; statusText: string; };
+interface ErrorProps {
+	status: number;
+	statusText: string;
+}
+
+function errorInfo({ error }: { error: ErrorProps }) {
+	return (
+		<p className="tw-text-slate-600">
+			<i>{`${error.status} ${error.statusText}`}</i>
+		</p>
+	);
+}
+
+function ErrorPage() {
+	const error = useRouteError() as { status: number; statusText: string };
 
 	return (
 		<div className="tw-w-full tw-h-screen tw-flex tw-justify-center tw-items-center tw-flex-col tw-text-slate-900">
 			<h1 className="tw-text-2xl tw-mb-4 tw-font-bold">Oops!</h1>
 			<p className="tw-mb-2">Sorry, an unexpected error has occurred.</p>
-			<p className="tw-text-slate-600">
-                <i>{`${error.status} ${error.statusText}`}</i>
-			</p>
+			{error.statusText ? <errorInfo error={error} /> : <></>}
 		</div>
 	);
 }
+
+export default ErrorPage;
